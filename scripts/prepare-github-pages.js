eadd253310f415d305c8c547349009844d10d867
Wesh Alkhutta Developer <dev@weshalkhutta.sa>
2026-08-13 23:47:15 +0300

    fix(deploy): update index.html meta viewport tag to enforce interactive-widget=overlays-content

--- scripts/prepare-github-pages.js
+++ scripts/prepare-github-pages.js
@@ -35,7 +35,7 @@ const indexHtmlContent = `<!DOCTYPE html>
 <html lang="ar" dir="rtl">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover, interactive-widget=overlays-content" />
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
